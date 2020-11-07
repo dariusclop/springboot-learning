2dbeefffbe63fab1ac2e8e8893bf0dcf6d1d7c9c
unknown <clop_darius@yahoo.com>
2020-11-08 01:19:34 +0200

    first commit

---
+++ src/main/java/com/lab2/cbse/CbseApplication.java
@@ -0,0 +1,13 @@
+package com.lab2.cbse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbseApplication.class, args);
+	}
+
+}
